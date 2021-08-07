0bb674353757f3e072f0e05e81e40957544b3150
tresnawidiyaman <tresna.widiyaman@gmail.com>
2021-08-07 11:35:15 +0700

    Update Installation

--- src/daumalapps.sh
+++ src/daumalapps.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sudo /opt/lampp/bin/mysql.server start
-sudo nodemon -x 'node /home/$USER/daumalapps/app.js || touch /home/$USER/daumalapps/app.js'
+sudo nodemon -x 'node /home/daumalapps/app.js || touch /home/daumalapps/app.js'
