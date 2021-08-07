d685b3530e43d3478561fdb90e8caf1e058f7759
tresnawidiyaman <tresna.widiyaman@gmail.com>
2021-08-07 11:19:02 +0700

    Update Installation

--- installapss.sh
+++ installapss.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-
+echo "Start Installation Autostar DaumalApps"
+echo "Please Wait"
+echo ". . . . . ."
 sudo mv src/daumalapps.sh /usr/local/bin/
 sudo mv src/daumalapps.service /etc/systemd/system/
-sudo chmod 744 /usr/local/bin/daumalapps.sh
-sudo chmod 664 /etc/systemd/system/daumalapps.service
+sudo chmod 755 /usr/local/bin/daumalapps.sh
+sudo chmod 755 /etc/systemd/system/daumalapps.service
 sudo systemctl daemon-reload
 sudo systemctl enable daumalapps.service
-echo "Installation Succesfull"
+echo "Installation Succesfull, please reboot your system !"
--- src/daumalapps.sh
+++ src/daumalapps.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sudo /opt/lampp/bin/mysql.server start
-sudo /home/$USER/daumalapps/nodemon -x 'node app.js || touch app.js'
+sudo nodemon -x 'node /home/$USER/daumalapps/app.js || touch /home/$USER/daumalapps/app.js'
